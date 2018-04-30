02cb2f16a08ba549f7bd3f6ae71cf33bc15c2721
clickomat <sconnell306@gmail.com>
2018-04-30 14:18:10 -0600

    pics wots show up

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// ===============================================================================
+var friends = [
+    {
+        "name": "alf",
+        "photo": "../assets/images/alf.jpg",
+        "scores": [
+            // 21
+            5,
+            3,
+            4,
+            4,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Animal",
+        "photo": "../assets/images/animal.jpg",
+        "scores": [
+            // 22
+            2,
+            5,
+            1,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Falkor",
+        "photo": "../assets/images/falkor.jpeg",
+        "scores": [
+            // 13
+            1,
+            1,
+            2,
+            4,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "Harry",
+        "photo": "../assets/images/harry.jpg",
+        "scores": [
+            // 19
+            2,
+            1,
+            5,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Oscar",
+        "photo": "../assets/images/oscar.jpg",
+        "scores": [
+            // 18
+            1,
+            1,
+            2,
+            5,
+            5,
+            4
+        ]
+    },
+    {
+        "name": "paddington",
+        "photo": "../assets/images/paddington.jpg",
+        "scores": [
+            // 17
+            1,
+            5,
+            5,
+            1,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Pan",
+        "photo": "../assets/images/pan.jpg",
+        "scores": [
+            // 11
+            1,
+            1,
+            5,
+            1,
+            1,
+            2
+        ]
+    },
+]
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
